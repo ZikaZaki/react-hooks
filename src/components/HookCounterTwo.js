29472060cdc13e2db2ebe10bc98afe5afc2bb5b1
ZikaZaki <17160061@su.edu.ye>
2023-05-21 23:41:12 +0300

    added incrementByFive method to HooksCounterTwo

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -4,12 +4,19 @@ function HookCounterTwo() {
   const initialCount = 0
   const [count, setCount] = useState(initialCount)
 
+  const incrementByFive = () => {
+    for (let i = 0; i < 5; i++) {
+        setCount(count + 1)
+    }
+  }
+
   return (
     <div>
         Count: {count}
         <button onClick={() => setCount(initialCount)}>Reset</button>
         <button onClick={() => setCount(count + 1)}>Increment</button>
         <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={incrementByFive}>Increment By 5</button>
     </div>
   )
 }
