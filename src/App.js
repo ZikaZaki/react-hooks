9c939fd79d5e7e8001023f4701ca243eedf79b2f
ZikaZaki <17160061@su.edu.ye>
2023-05-21 23:51:19 +0300

    commented ClassCounterTwo in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <ClassCounterTwo />
+      {/* <ClassCounterTwo /> */}
       {/* <HookCounter /> */}
       <HookCounterTwo />
     </div>
