8551a0b7b90b5845ec73f2e2c48e2cb24b0933c8
ZikaZaki <17160061@su.edu.ye>
2023-05-21 23:25:09 +0300

    added prologue comments to HooksCounter component

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react"
-
+/*
+=============================Rules of Hooks=============================
+1- Only call Hooks at the Top Level.
+Don't call Hooks inside loops, conditions, or nested functions.
+2- Only call Hooks from React Functions.
+Call them from within React functional components and not just any 
+regular JavaScript function.
+========================================================================
+*/
 function HookCounter() {
   const [count, setCount] = useState(0)
 
