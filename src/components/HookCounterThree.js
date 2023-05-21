073c0a2d4caa663168d6d9ea747c94466143a48e
ZikaZaki <17160061@su.edu.ye>
2023-05-22 00:00:06 +0300

    update HookCounterThree

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -6,8 +6,18 @@ function HookCounterThree() {
   return (
     <div>
         <form>
-            <input type="text" />
-            <input type="text" />
+            <input
+              type="text"
+              value={name.firstName}
+              onChange={(e) => setName({ firstName: e.target.value })}
+            />
+            <input
+              type="text"
+              value={name.lastName}
+              onChange={(e) => setName({ lastName: e.target.value })}
+            />
+            <h2>Your first name is: {name.firstName}</h2>
+            <h2>Your last name is: {name.lastName}</h2>
         </form>
     </div>
   )
