967de3787b0e8921415bdde133bd2246abc638ed
ZikaZaki <17160061@su.edu.ye>
2023-05-22 17:39:58 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ClassCounter from "./components/ClassCounter";
 import ClassCounterThree from "./components/ClassCounterThree";
 import ClassCounterTwo from "./components/ClassCounterTwo";
+import ClassMouse from "./components/ClassMouse";
 import HookCounter from "./components/HookCounter";
 import HookCounterFour from "./components/HookCounterFour";
 import HookCounterThree from "./components/HookCounterThree";
@@ -10,10 +11,13 @@ import HookCounterTwo from "./components/HookCounterTwo";
 function App() {
   return (
     <div className="App">
+    {/* ========================CLASS COMPONENTS======================== */}
       {/* <ClassCounter /> */}
       {/* <ClassCounterTwo /> */}
       {/* <ClassCounterThree /> */}
-      <HookCounter />
+      <ClassMouse />
+    {/* ======================FUNCTIONAL COMPONENTS====================== */}
+      {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
