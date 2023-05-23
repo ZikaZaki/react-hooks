955c176dedc919748a606f562db2211387f4dbee
ZikaZaki <17160061@su.edu.ye>
2023-05-24 02:23:15 +0300

    add IntervalHookCounter component

---
+++ src/components/IntervalHookCounter.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react"
+
+function IntervalHookCounter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>IntervalHookCounter</div>
+  )
+}
+
+export default IntervalHookCounter
