6603c969bbbf48d4779204dfb36a95bf3179766b
ZikaZaki <17160061@su.edu.ye>
2023-05-23 23:54:41 +0300

    update summary comment

--- src/components/HookMouse.js
+++ src/components/HookMouse.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react"
-/*============================useEffect Hook===================================
+/* ============================useEffect Hook===================================
 The useEffect hook  lets you perform side effects in functional components.
 It is a close replacement for componentDidMount, componentDidUpdate, and
 componentWillUnmount.
 --------------------------------------------------------------------------------
-# Rules of using useEffect Hook:
-1- It should be used in the component's scope.
-2- It should be used only once.
-3- It should be used in functional component only.
-4- It should be used after the state has been updated.
+* # Rules of using useEffect Hook:
+* 1- It should be used in the component's scope.
+* 2- It should be used only once.
+* 3- It should be used in functional component only.
+* 4- It should be used after the state has been updated.
 --------------------------------------------------------------------------------
 */
 function HookMouse() {
