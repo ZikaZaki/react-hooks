c409277708bf930f63bde464725675b387d1b2da
ZikaZaki <17160061@su.edu.ye>
2023-05-23 23:57:20 +0300

    update summary comment

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react"
-/*
-=============================Rules of Hooks=============================
-1- Only call Hooks at the Top Level.
-Don't call Hooks inside loops, conditions, or nested functions.
-2- Only call Hooks from React Functions.
-Call them from within React functional components and not just any 
-regular JavaScript function.
+/* =============================Rules of Hooks=============================
+* 1- Only call Hooks at the Top Level. 
+* 2- Don't call Hooks inside loops, conditions, or nested functions.
+* 3- Only call Hooks from React Functions.
+* 4- Call them from within React functional components and not just any 
+*    regular JavaScript function.
 ========================================================================
 */
 function HookCounter() {
