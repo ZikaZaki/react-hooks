2048935afc3e8c1e5a53a65d8158b0af656b99c2
ZikaZaki <17160061@su.edu.ye>
2023-05-23 23:55:27 +0300

    update summary comment

--- src/components/HookCounterFour.js
+++ src/components/HookCounterFour.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react"
-/*===============================Summary - useState=================================
+/* ===============================Summary - useState=================================
 The useState Hook lets you add state to functional components.
 In classes, the state is always an Object. With the useState Hook, the state doesn't
 have to be an object, it can be an array, a number, a boolean, or even a string.
 ------------------------------------------------------------------------------------
-NOTE: The useState Hook returns an array with 2 elements:
-1- The first element is the current value of the state.
-2- The second element is a state setter function.
+* NOTE: The useState Hook returns an array with 2 elements:
+* 1- The first element is the current value of the state.
+* 2- The second element is a state setter function.
 In case the (new) state value depends on the (previous) state value, you can pass
 a function to the setter function which will receive the previous state value as a
 its argument.
 ------------------------------------------------------------------------------------
-CAREFUL!!!: When dealing with objects or arrays, always make sure to SPREAD your
+* CAREFUL!!!: When dealing with objects or arrays, always make sure to SPREAD your
 state variable and then call the setter function.
 ==================================================================================*/
 
