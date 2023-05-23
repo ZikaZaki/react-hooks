046469a1ea61f2c5ab07bd90dcd032f717ad2c83
ZikaZaki <17160061@su.edu.ye>
2023-05-23 23:48:31 +0300

    add prologue comment

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react"
-
+/*=================================useState=================================
+# Rules of using (useState) Hook:
+1- It should be used in the component's scope.
+2- It should be used only once.
+3- It should be used in functional component only.
+------------------------------------------------------------------------------
+NOTE: The key difference between (useState) Hook in a functional component and
+(this.setState) in class component, is that (useState) will not automatically
+merge and update the state and you have to do it manually, whereas (this.setState)
+in class component will automatically merge and update the state.
+===============================================================================*/
 function HookCounterTwo() {
   const initialCount = 0
   const [count, setCount] = useState(initialCount)
