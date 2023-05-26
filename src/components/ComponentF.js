d49ba05372b4cbdab3c532287b8b3a622e70bd6c
ZikaZaki <17160061@su.edu.ye>
2023-05-26 03:55:31 +0300

    add summary comments

--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,6 +1,17 @@
 import React from "react"
 import { UserContext, ChannelContext } from "../App"
-
+/*
+*NOTE:
+*  We can use the Consumer component to consume the context values.
+*  The Consumer component accepts a function as a child.
+*  The function is called with the context value as an argument.
+*  The return value of the function is what gets rendered.
+*  The value prop is passed to the Provider component.
+--------------------------------------------------------------------------------
+*BEST PRACTICES:
+*  Use the Consumer component if you need to consume more than one context in a component.
+*  Use the static contextType property if you only need to consume a single context in a component.
+*/
 function ComponentF() {
   return (
     <div>
