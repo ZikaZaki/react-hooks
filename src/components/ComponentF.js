29854c51bfb29043867df9afbeb30822871331ce
ZikaZaki <17160061@su.edu.ye>
2023-05-26 03:52:04 +0300

    consumed ChannelContext in ComponentF

--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { UserContext } from "../App"
+import { UserContext, ChannelContext } from "../App"
 
 function ComponentF() {
   return (
@@ -7,7 +7,20 @@ function ComponentF() {
       <UserContext.Consumer>
         {
             user => {
-              return <div>User context value: {user}</div>
+              return (
+                <ChannelContext.Consumer>
+                  {
+                    channel => {
+                      return (
+                        <div>
+                          <h2>User context value: {user}</h2>
+                          <h2>Channel context value: {channel}</h2>
+                        </div>
+                      )
+                    }
+                  }
+                </ChannelContext.Consumer>
+              )
             }
         }
       </UserContext.Consumer>
