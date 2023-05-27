f444b0ed452f0be37d7fa1f95804f848f7cdb771
ZikaZaki <17160061@su.edu.ye>
2023-05-28 01:06:05 +0300

    add ComponentF to ComponentE

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,15 +1,11 @@
-import React, { useContext } from "react"
+import React from "react"
 import ComponentF from "./ComponentF"
-import { UserContext, ChannelContext } from "../App"
 
 function ComponentE() {
-  const user = useContext(UserContext)
-  const channel =useContext(ChannelContext)
-
+  
   return (
     <div>
-      user: {user} - channel: {channel}
-      {/* <ComponentF /> */}
+      <ComponentF />
     </div>
   )
 }
