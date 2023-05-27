c821772dba2958660b4c19da777dac0765c1d689
ZikaZaki <17160061@su.edu.ye>
2023-05-28 01:34:31 +0300

    consumed CountContext

--- src/components/ComponentD.js
+++ src/components/ComponentD.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
+import { CountContext } from "../App"
 
 function ComponentD() {
+  const countContext = useContext(CountContext)
+
   return (
-    <div>ComponentD</div>
+    <div>
+        <h2>Component D</h2>
+        <button onClick={() => countContext.countDispatch({type: "INCREMENT", value: 1})}>Increment</button>
+        <button onClick={() => countContext.countDispatch({type: "DECREMENT", value: 1})}>Decrement</button>
+        <button onClick={() => countContext.countDispatch({type: "RESET"})}>Reset</button>
+    </div>
   )
 }
 
