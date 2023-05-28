ea95b7ddd9903ca93286545ab91e76f97735b64a
ZikaZaki <17160061@su.edu.ye>
2023-05-29 02:23:33 +0300

    update console.log

--- src/components/Employee/Button.js
+++ src/components/Employee/Button.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function Button({ handleClick, children }) {
-  console.log('Rendering button - ', children)
+  console.log('Rendering Button - ', children)
 
   return (
     <button onClick={handleClick}>
