481111c8e8c272ed156322b40143d9a96b187062
ZikaZaki <17160061@su.edu.ye>
2023-05-29 01:28:39 +0300

    update errorMessage in DataFetchingOne & DataFetchingTwo components

--- src/components/DataFetchingTwo.js
+++ src/components/DataFetchingTwo.js
@@ -28,6 +28,7 @@ const reducer = (state, action) => {
 
 function DataFetchingTwo() {
   const [postState, dispatch] = useReducer(reducer, initialState)
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts/1")
       .then(response => {
