a9a1c8a87edc728a435e268d30aeea90df5e3055
ZikaZaki <17160061@su.edu.ye>
2023-05-29 01:51:41 +0300

    add Title, Button, & Count components to Employee/ParentComponent

--- src/components/Employee/ParentComponent.js
+++ src/components/Employee/ParentComponent.js
@@ -4,8 +4,25 @@ import Button from "./Button"
 import Title from "./Title"
 
 function ParentComponent() {
+  const [age, setAge] = useState(25)
+  const [salary, setSalary] = useState(50000)
+
+  const incrementAge = () => {
+    setAge(age + 1)
+  }
+
+  const incrementSalary = () => {
+    setSalary(salary + 1000)
+  }
+
   return (
-    <div>ParentComponent</div>
+    <div>
+      <Title />
+      <Count text="Age" count={age} />
+      <Button handleClick={incrementAge}>Increment Age</Button>
+      <Count text="Salary" count={salary} />
+      <Button handleClick={incrementSalary}> Increment Salary</Button>
+    </div>
   )
 }
 
