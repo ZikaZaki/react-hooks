85820dad6c8865a4cc1ca9f8bb8af33521d0e6a3
ZikaZaki <17160061@su.edu.ye>
2023-05-30 02:48:38 +0300

    add summary comment

--- src/components/FocusInput.js
+++ src/components/FocusInput.js
@@ -1,4 +1,10 @@
 import React, { useEffect, useRef } from "react"
+/**
+ * useRef Hook is used to access DOM nodes or HTML elements.
+ * useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue).
+ * The returned object will persist for the full lifetime of the component.
+ * 
+ */
 
 function FocusInput() {
   const inputRef = useRef(null)
