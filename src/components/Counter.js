9b836161184b8d03c237112ebce46504160cba28
ZikaZaki <17160061@su.edu.ye>
2023-05-29 03:18:43 +0300

    update Counter component with useMemo hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useMemo } from "react"
 
 function Counter() {
   const [counterOne, setCounterOne] = useState(0)
@@ -12,10 +12,18 @@ function Counter() {
     setCounterTwo(counterTwo + 1)
   }
 
+  const isEven = useMemo(() => {
+    let i = 0
+    while( i < 2000000000) i++
+    
+    return counterOne % 2 === 0
+  }, [counterOne])
+
   return (
     <div>
       <div>
         <button onClick={incrementOne}>Count One: {counterOne}</button>
+        <span>{isEven ? ' Even' : ' Odd'}</span>
       </div>
       <div>
         <button onClick={incrementTwo}>Count Two: {counterTwo}</button>
