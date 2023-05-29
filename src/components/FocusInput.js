edcb68bb6c61d102ef02096121b6a90f3267dd19
ZikaZaki <17160061@su.edu.ye>
2023-05-30 02:49:42 +0300

    update summary comment

--- src/components/FocusInput.js
+++ src/components/FocusInput.js
@@ -2,7 +2,27 @@ import React, { useEffect, useRef } from "react"
 /**
  * useRef Hook is used to access DOM nodes or HTML elements.
  * useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue).
- * The returned object will persist for the full lifetime of the component.
+ * The returned object will persist for the full lifetime of the component. This means that,
+ * unlike React's built-in ref, it won't be garbage collected.
+ *
+ * @returns {object}
+ * @example
+ * import React, { useRef } from 'react'
+ * 
+ * function TextInputWithFocusButton() {
+ *  const inputEl = useRef(null)
+ * const onButtonClick = () => {
+ * inputEl.current.focus()
+ * }
+ * return (
+ * <>
+ * <input ref={inputEl} type="text" />
+ * <button onClick={onButtonClick}>Focus the input</button>
+ * </>
+ * )
+ * }
+ * 
+ * export default TextInputWithFocusButton
  * 
  */
 
