5abf42dc1f02bdcb28ba09b2789e4f391be82054
ZikaZaki <17160061@su.edu.ye>
2023-05-29 03:08:14 +0300

    update Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,26 @@
 import React, { useState } from "react"
 
 function Counter() {
+  const [counterOne, setCounterOne] = useState(0)
+  const [counterTwo, setCounterTwo] = useState(0)
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1)
+  }
+
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 1)
+  }
+
   return (
-    <div></div>
+    <div>
+      <div>
+        <button onClick={incrementOne}>Count One: {counterOne}</button>
+      </div>
+      <div>
+        <button onClick={incrementTwo}>Count Two: {counterTwo}</button>
+      </div>
+    </div>
   )
 }
 
