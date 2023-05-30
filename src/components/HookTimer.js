1201cdb64de506d2d14b493ee55fb267d92b9ccf
ZikaZaki <17160061@su.edu.ye>
2023-05-30 23:41:57 +0300

    add useState & useEffect hooks

--- src/components/HookTimer.js
+++ src/components/HookTimer.js
@@ -1,6 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 function HookTimer() {
+  const [timer, setTimer] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => prevTimer + 1)
+    }, 1000)
+  })
+
   return (
     <div>HookTimer</div>
   )
