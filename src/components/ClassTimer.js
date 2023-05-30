9ac21ea98cbda5a8d983da636ec234e5e009f9aa
ZikaZaki <17160061@su.edu.ye>
2023-05-30 23:33:17 +0300

    add componentDidMount lifecycle method

--- src/components/ClassTimer.js
+++ src/components/ClassTimer.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react"
 
 class ClassTimer extends Component {
+  interval
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       timer: 0
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({ timer: prevState.timer + 1 }))
+    }, 1000)
+  }
+
   render() {
     return (
       <div>ClassTimer</div>
