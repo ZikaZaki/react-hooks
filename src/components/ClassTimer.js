d1b0c65e70311cefe67025c27a1a030e4fba41b1
ZikaZaki <17160061@su.edu.ye>
2023-05-30 23:30:51 +0300

    add ClassTimer component

---
+++ src/components/ClassTimer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class ClassTimer extends Component {
+  render() {
+    return (
+      <div>ClassTimer</div>
+    )
+  }
+}
+
+export default ClassTimer
