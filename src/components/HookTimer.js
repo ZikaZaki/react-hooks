1a2a01af2e45aa6a59521ad7df8e0315886e8f37
ZikaZaki <17160061@su.edu.ye>
2023-05-31 00:07:10 +0300

    update summary comment

--- src/components/HookTimer.js
+++ src/components/HookTimer.js
@@ -4,11 +4,15 @@ import React, { useState, useEffect, useRef } from "react"
  * useRef Hook allows you to create a mutable object that persists for the lifetime of the component
  * useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue)
  * The returned object will persist for the full lifetime of the component.
- * useRef() is like a “box/container” that can hold a mutable value in its .current property.
+ * useRef() is like a generic “box/container” that can hold a mutable value in its .current property.
  * useRef() is useful for more than the ref attribute. It’s handy for keeping any mutable 
  * value around similar to how you’d use instance fields in classes. This works because useRef() creates a plain 
  * JavaScript object. The only difference between useRef() and creating a {current: ...} object yourself is that 
- * useRef will give you the same ref object on every render. 
+ * useRef will give you the same ref object on every render. One use case for useRef is to manage focus, text selection, 
+ * or media playback, because you can store a reference to a DOM node or React element in a ref. 
+ * Moreover,useRef is used for managing form data, where the mutable ref object can be used to store the form’s data between
+ * renders without triggering a rerender on its own. Another simple use case is clearing timeouts and intervals from
+ * useEffect() hook via event handlers.
  * @see https://reactjs.org/docs/hooks-reference.html#useref)
  * ---------------------------------------------------------------------------------------------------------
  */
